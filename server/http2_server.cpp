0531731f93caf96f68a838b0c36a2f7d5346dccd
Francesco Boffa <fra.boffa@gmail.com>
2014-05-15 01:10:59 +0200

    HTTP2 bootstrap, client validation, sending preface and receiving frames.

--- server/http2_server.cpp
+++ server/http2_server.cpp
@@ -1,15 +1,25 @@
 #include "http2_server.h"
 #include "http2_frame.h"
-#include <iostream>
 
+#include <iostream>
 using namespace std;
 
 Http2Server::Http2Server(SockStream &in, ClientSocket &out) : in(in), out(out) {
 }
 
 void Http2Server::run() {
+	cout << "Greeat" << endl;
 	read_first_settings();
 	send_preface();
+
+	while (true) {
+		Http2Frame f = read_frame();
+		cout << "Received frame. Type: " << (int)f.header.type << endl;
+		if (f.header.type == FrameTypes::GoAway) {
+			cout << "Received GoAway. Error: " << *((unsigned int *)&f.data[4]) << endl;
+			break;
+		}
+	}
 }
 
 void Http2Server::read_first_settings() {
@@ -17,6 +27,8 @@ void Http2Server::read_first_settings() {
 
 	if (f.header.type != FrameTypes::Settings) {
 		throw "This is not HTTP2!";
+	} else {
+		cout << "Got settings! Valid HTTP2" << endl;
 	}
 }
 
@@ -30,9 +42,9 @@ Http2Frame Http2Server::read_frame() {
 	Http2Frame frame;
 
 	in.read_bytes((unsigned char *) &frame.header, sizeof(frame.header));
-	if (frame.header.length != 0) {
-		frame.data = new unsigned char[frame.header.length];
-		in.read_bytes(frame.data, frame.header.length);
+	if (frame.header.get_length() != 0) {
+		frame.data = new unsigned char[frame.header.get_length()];
+		in.read_bytes(frame.data, frame.header.get_length());
 	}
 
 	return frame;
